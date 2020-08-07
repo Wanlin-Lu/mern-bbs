65da5ccece303ddc4f11bcfff05dd742b3f125e3
Wanlin-Lu <luwanlin@126.com>
2020-08-07 23:40:20 +0800

    utils: request

---
+++ client/src/utils/request.js
@@ -0,0 +1,24 @@
+export const call = async (url, method = "GET", body = null, headers = { } ) => {
+  try {
+    const response = await fetch(url, {
+      method,
+      body,
+      headers: {
+        ...headers,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+
+    const responseData = await response.json()
+
+    if (!response.ok) {
+      return { error: { message: "Request failed due to server error." } }
+    }
+
+    return responseData
+  } catch (err) {
+    return { error: { message: "Request failed." } };
+  }
+}
+
