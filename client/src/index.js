8373a696975dee92d16a81b031a0a889465154ed
Wanlin-Lu <luwanlin@126.com>
2020-08-08 15:51:46 +0800

    App/router

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import configureStore from './redux/configureStore'
+import App from './containers/App'
 
 const store = configureStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <h1>BBS app</h1>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
