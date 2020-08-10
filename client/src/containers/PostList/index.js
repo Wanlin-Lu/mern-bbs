a35bb91c66570030af13cc4634ab22ea5248da33
Wanlin-Lu <luwanlin@126.com>
2020-08-10 16:02:52 +0800

    PostList

--- client/src/containers/PostList/index.js
+++ client/src/containers/PostList/index.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
-const Auth = () => (
-  <h1>PostList</h1>
-)
+import { actions as uiActions, isAddDialogOpen } from '../../redux/modules/ui'
+import { actions as postsActions } from '../../redux/modules/posts'
+import { getPostListWithAuthors } from '../../redux/modules'
+import { getLoggedUser } from '../../redux/modules/auth'
 
-export default Auth
+import PostsView from './components/PostsView'
+import PostEditor from '../Post/components/PostEditor'
+import './style.css'
+
+const PostList = ({user, posts, addDialogOpen, openAddDialog, closeAddDialog, createPost, fetchPostList }) => {
+
+  useEffect(() => {
+    fetchPostList()
+  }, [fetchPostList])
+
+  const handleSave = data => {
+    createPost(data.title, data.content)
+  }
+  
+  return (
+    <div className="postList">
+      <div>
+        <h2>话题列表</h2>
+        {user.userId ? (
+          <button onClick={openAddDialog}>发帖</button>
+        ) : null}
+      </div>
+      {addDialogOpen && <PostEditor onSave={handleSave} onCancel={closeAddDialog} />}
+      <PostsView posts={posts} />
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  posts: getPostListWithAuthors(state),
+  addDialogOpen: isAddDialogOpen(state),
+  user: getLoggedUser(state)
+})
+
+const mapDispatchToProps = dispatch => ({
+  ...bindActionCreators(postsActions, dispatch),
+  ...bindActionCreators(uiActions, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostList)
