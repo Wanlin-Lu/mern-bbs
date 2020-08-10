e17621483edc7d68d327031dad06a307d5543c5f
Wanlin-Lu <luwanlin@126.com>
2020-08-10 09:50:14 +0800

    Home start

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Route, useRouteMatch, useLocation } from 'react-router-dom'
+import { actions as authActions, getLoggedUser } from '../../redux/modules/auth'
 
-const Auth = () => (
-  <h1>Auth</h1>
-)
+import Header from '../../components/Header'
+import PostList from '../PostList'
+import Post from '../Post'
 
-export default Auth
+const Home = ({ user, logout }) => {
+  const match = useRouteMatch()
+  const location = useLocation()
+  const username = user && user.username ? user.username : ""
+
+  return (
+    <div>
+      <Header
+        username={username}
+        location={location}
+        onLogout={logout}
+      />
+      <Route
+        path={match.url}
+        exact
+        render={props => <PostList {...props} />}
+      />
+      <Route
+        path={`${match.url}/:id`}
+        render={props => <Post {...props} />}
+      />
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  user: getLoggedUser(state)
+})
+
+export default connect(mapStateToProps,authActions)(Home)
---
+++ client/src/containers/PostList/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Auth = () => (
+  <h1>PostList</h1>
+)
+
+export default Auth
