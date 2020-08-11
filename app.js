01a62a90affa20f49069b2c42f1533b36620e031
Wanlin-Lu <luwanlin@126.com>
2020-08-11 17:13:28 +0800

    mongodb & connect and 404 route

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb')
 
 const app = express()
 
@@ -11,6 +12,22 @@ app.get('/', (req, res, next) => {
   res.json({message: "app is running."})
 })
 
-app.listen(5000, () => {
-  console.log(`Listening on port 5000.`)
+app.use("*", (req, res) => res.status(404).json({ error: "not found"}))
+
+MongoClient.connect(
+  process.env.DB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    poolSize: 50,
+    wtimeout: 2500,
+  }
+).catch(err => {
+  console.log(err.stack)
+  process.exit(1)
+}).then(async client => {
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`)
+  })
 })
