335aa4ce3fff2b4de1dca210e14a00d466cfbf3e
Wanlin-Lu <luwanlin@126.com>
2020-08-31 15:27:28 +0800

    BU: checkAuth used in comments

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,8 +1,6 @@
 const jwt = require("jsonwebtoken");
 const commentDAO = require('../dao/commentDAO')
 
-
-
 const getCommentList = async (req, res, next) => {
   const pid = req.params.id;
   const comments = await commentDAO.getComments(pid);
@@ -12,25 +10,6 @@ const getCommentList = async (req, res, next) => {
 
 const createComment = async (req, res, next) => {
   try {
-    const userJwt = req.get("Authorization").slice("Bearer ".length);
-
-    const decodedToken = jwt.verify(
-      userJwt,
-      process.env.SECRET_KEY,
-      (error, res) => {
-        if (error) {
-          return { error };
-        }
-        return { ...res };
-      }
-    );
-
-    var { error } = decodedToken;
-    if (error) {
-      res.status(401).json({ error });
-      return;
-    }
-
     const commentFromBody  = req.body;
     const createCommentResult = await commentDAO.createComment(commentFromBody)
 
--- routers/comment-router.js
+++ routers/comment-router.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const commentControllers = require("../controllers/comment.controller");
+const checkAuth = require('../middleware/check-auth')
 
 const router = express.Router();
 
 router.get("/:id", commentControllers.getCommentList);
+
+router.use(checkAuth)
 
 router.post("/", commentControllers.createComment);
 
